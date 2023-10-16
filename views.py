a3014af5a63f6348972acff7ed920bf06366b543
Pravargav <96449025+Pravargav@users.noreply.github.com>
2023-10-16 08:57:44 +0530

    Update views.py

--- views.py
+++ views.py
@@ -5,6 +5,12 @@ def index(request):
     context={}
     return render(request, 'takeTest/index.html', context)
 
+def subject(request):
+    context={}
+    return render(request, 'takeTest/subject.html', context)
 
+def theory(request):
+    context={}
+    return render(request, 'takeTest/theory.html', context)
 
 
