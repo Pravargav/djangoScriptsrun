a9dc1e58b55d143f049c81f56efdd65a6f0620ce
Pravargav <96449025+Pravargav@users.noreply.github.com>
2023-10-16 09:18:05 +0530

    Update urls.py

--- urls.py
+++ urls.py
@@ -1,6 +1,12 @@
+
+
 from django.urls import path,include
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index')   
+    path('', views.index, name='index'),
+    path('subject/', views.subject, name='subject'),
+    path('subject/',include([
+        path('theory/', views.theory),
+    ])),
 ] 
